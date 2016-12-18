2d8ba7bf6cbec1e2f6271d7c91ce09880f527cc6
lam32767 <kurt.swendson@hotmail.com>
2016-12-17 19:23:14 -0600

    mock tests

--- TestProject/DbIntegrationTest.cs
+++ TestProject/DbIntegrationTest.cs
@@ -9,44 +9,22 @@ namespace TestProject
     [TestClass]
     public class DbIntegrationTest
     {
-
+        IVideoContext _context = new VideoContext();
 
         [TestMethod]
-        public void CantFindAnything()
+        public void CantFindInvalid()
         {
-            using (var context = new VideoContext())
+            using (var context = _context)
             {
-                Video theRecord = context.Videos.Find("wZZ7oFKsKzY");
+                Video theRecord = context.Videos.Find("invalid_key_that_doesnt_exist");
                 Assert.IsNull(theRecord);
             }
         }
 
-
-
         [TestMethod]
-        public void InsertAndDelete()
+        public void DeleteNyanCat()
         {
-            var newvid = new Video
-            {
-                Id = "wZZ7oFKsKzY",
-                ChannelTitle = "Meow",
-                Comment = "Comment",
-                Dislikes = 10,
-                Likes = 11,
-                PublishDate = DateTime.Now.AddMonths(-8).ToShortDateString(),
-                Rating = "*****",
-                Title = "Nyan cat 10 hour"
-            };
-            using (var context = new VideoContext())
-            {
-                context.Videos.Add(newvid);
-                context.SaveChanges();
-                Video theRecord = context.Videos.Find("wZZ7oFKsKzY");
-                Assert.IsNotNull(theRecord);
-            }
-
-
-            using (var context = new VideoContext())
+            using (var context = _context)
             {
                 Video theRecord = context.Videos.Find("wZZ7oFKsKzY");
                 Video theRecord2 = null;
@@ -59,6 +37,45 @@ namespace TestProject
                     Assert.IsNull(theRecord2);
                 }
                 Assert.IsNull(theRecord2);
+            }
+        }
+
+        [TestMethod]
+        public void Insert()
+        {
+            ////////////////just make sure nyan is gone
+            using (var context = _context)
+            {
+                Video theRecord = context.Videos.Find("wZZ7oFKsKzY");
+                Video theRecord2 = null;
+
+                if (theRecord != null)
+                {
+                    context.Videos.Remove(theRecord);
+                    context.SaveChanges();
+                    theRecord2 = context.Videos.Find("wZZ7oFKsKzY");
+                    Assert.IsNull(theRecord2);
+                }
+                Assert.IsNull(theRecord2);
+
+                ////////////now add nyan
+                var newvid = new Video
+                {
+                    Id = "wZZ7oFKsKzY",
+                    ChannelTitle = "Meow",
+                    Comment = "Comment",
+                    Dislikes = 10,
+                    Likes = 11,
+                    PublishDate = DateTime.Now.AddMonths(-8).ToShortDateString(),
+                    Rating = "*****",
+                    Title = "Nyan cat 10 hour"
+                };
+
+                //////////prove nyan was added
+                context.Videos.Add(newvid);
+                context.SaveChanges();
+                Video theRecord3 = context.Videos.Find("wZZ7oFKsKzY");
+                Assert.IsNotNull(theRecord3);
             }
         }
     }
---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using VideoModel;
+using System.Data.Entity;
+using System.Linq;
+
+namespace TestProject
+{
+    [TestClass]
+    public class Mockery
+    {
+        [TestMethod]
+        public void TestWithMock()
+        {
+            var mocklist=new List<Video>
+                {
+                    new VideoModel.Video() { ChannelTitle="title1", Comment="comment1", Dislikes=1, Id="id1", Likes=1, Rating="*", Title="title1", PublishDate=DateTime.Today.ToShortDateString() },
+                    new VideoModel.Video() { ChannelTitle="title2", Comment="comment2", Dislikes=2, Id="id2", Likes=2, Rating="**", Title="title2", PublishDate=DateTime.Today.ToShortDateString() }
+                }.AsQueryable();
+
+            var mockSet = new Mock<System.Data.Entity.DbSet<Video>>();
+            mockSet.As<IQueryable<Video>>().Setup(m => m.Provider).Returns(mocklist.Provider);
+            mockSet.As<IQueryable<Video>>().Setup(m => m.Expression).Returns(mocklist.Expression);
+            mockSet.As<IQueryable<Video>>().Setup(m => m.ElementType).Returns(mocklist.ElementType);
+            mockSet.As<IQueryable<Video>>().Setup(m => m.GetEnumerator()).Returns(mocklist.GetEnumerator());
+            var mockContext = new Mock<DataAccess.VideoContext>();
+
+            mockContext.Setup(m => m.GetVideos()).Returns(mocklist);
+            List<Video> testoutput = mockContext.Object.GetVideos().ToList();
+            Assert.IsTrue(testoutput.Count == 2);
+
+            Video first = testoutput.First();
+            Assert.AreEqual(first.ChannelTitle, "title1");
+            Assert.AreEqual(first.Dislikes, 1);
+            Video last = testoutput.Last();
+            Assert.AreEqual(last.ChannelTitle, "title2");
+            Assert.AreEqual(last.Dislikes, 2);
+
+        }
+    }
+}
